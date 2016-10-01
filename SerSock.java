942fdad0d92d7870a77c4c1130ce5035ab893e13
Luis <lmatosgarcia14@outlook.com>
2016-10-01 10:52:00 -0400

    Loop for SerSock.java added to keep running

--- SerSock.java
+++ SerSock.java
@@ -10,21 +10,22 @@ import Socket.Estructura;
 public class SerSock{
   public static void main(String[] args) throws UnknownHostException, IOException, ClassNotFoundException {
     ServerSocket serverSocket = new ServerSocket(4444);
-    Socket socket = serverSocket.accept();
+    Socket socket;
 
     ServerSocket server;
     Socket connection;
+    while(true){
+        socket = serverSocket.accept();
+        PrintWriter out = new PrintWriter(socket.getOutputStream(),
+        true);
+        BufferedReader in = new BufferedReader(
+          new InputStreamReader(socket.getInputStream()));
 
-    PrintWriter out = new PrintWriter(socket.getOutputStream(),
-    true);
-    BufferedReader in = new BufferedReader(
-      new InputStreamReader(socket.getInputStream()));
+        String inputLine,outputLine;
+        while((inputLine = in.readLine()) != null) {
+           System.out.println(inputLine);
 
-    String inputLine,outputLine;
-    while((inputLine = in.readLine()) != null) {
-       System.out.println(inputLine);
-
-     }
-
+         }
+    }
 }
 }
